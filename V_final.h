978c1965c46c7a2eb52867689fe36914b3c43027
IanWark <wark@ualberta.ca>
2017-07-28 16:10:52 -0600

    Allocate V final based on sequence length not MAXSLEN

--- V_final.h
+++ V_final.h
@@ -9,15 +9,15 @@ class VM_final;
 class V_final{
 	public: 
 	// constructor
-	V_final();
+	V_final(int nb_nucleotides);
 	~V_final();
 	void setloops(s_energy_matrix *v, VM_final *vm);
 	int get_energy(int i, int j);
 	
 	char get_type (int i, int j);
-    // return the type at v_final(i,j)
+        // return the type at v_final(i,j)
     
-    void set_features(str_features *f){fres = f;}
+        void set_features(str_features *f){fres = f;}
 		
 	protected:
 	s_energy_matrix *v;
